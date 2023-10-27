a485d88c6c45f821842d8fd2a69301ba4bd15e7e
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2023-10-27 12:27:50 -0700

    added comment to asymmetric.py and symmetric.py

--- symmetric.py
+++ symmetric.py
@@ -1,15 +1,22 @@
 from cryptography.fernet import Fernet
 
+# generating symmetric key. it is 256 bits.
+# we can use it for 128 and 196 bits encryption
 key = Fernet.generate_key()
 
 
 def encode(msg, key):
+    # creating a cipher with the generated key
     cipher = Fernet(key)
+    # encoding message to bytes
     byte_msg = msg.encode("utf-8")
+    # returning the cipher-text
     return cipher.encrypt(byte_msg)
 
 def decode(msg, key):
+    # creating cipher with generated key
     cipher = Fernet(key)
+    # retrun the decrypted message
     return cipher.decrypt(msg).decode("utf-8")
 
 if __name__=="__main__":
