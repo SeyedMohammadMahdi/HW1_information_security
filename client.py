e488cbff9330b4627f6b7551c0f181ec9be7a681
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2023-10-27 04:40:57 -0700

    made a simple client srever chat app

--- client.py
+++ client.py
@@ -1,12 +1,43 @@
 import socket 
+from threading import Thread
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def receive():
+    host = "localhost"
+    port = 1235
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(1)
+    while(True):
+        conn, addr = server.accept()
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        conn.close()
+        if(data.decode() == "close"):
+            server.close()
+            break
 
-host = "localhost"
-port = 1234
+def send():
+    host = "localhost"
+    port = 1234
+    while(True):
+        sct  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sct.connect((host, port))
+            data = input()
+            sct.send(data.encode("utf-8"))
+            sct.close()
+            if(data == "close"):
+                return
+        except:
+            pass
 
-socket.connect((host, port))
-data = "hello"
-socket.send(data.encode())
 
-socket.close()
+sd = Thread(target=send)
+rc = Thread(target=receive)
+
+sd.start()
+rc.start()
+
+sd.join()
+rc.join()
+print("done")
--- server.py
+++ server.py
@@ -1,17 +1,58 @@
 import socket
+from threading import Thread
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host = "localhost"
+# port = 1234
 
-host = "localhost"
-port = 1234
+# server.bind((host, port))
 
-server.bind((host, port))
+# server.listen(1)
 
-server.listen(1)
+# conn, addr = server.accept()
 
-conn, addr = server.accept()
+# data = conn.recv(1024)
+# print(data.decode())
 
-data = conn.recv(1024)
-print(data.decode())
+# server.close()
 
-server.close()
+def receive():
+    host = "localhost"
+    port = 1234
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(1)
+    while(True):
+        conn, addr = server.accept()
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+        conn.close()
+        if(data.decode() == "close"):
+            server.close()
+            break
+
+def send():
+    host = "localhost"
+    port = 1235
+    while(True):
+        sct  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sct.connect((host, port))
+            data = input()
+            sct.send(data.encode("utf-8"))
+            sct.close()
+            if(data == "close"):
+                return
+        except:
+            pass
+
+
+sd = Thread(target=send)
+rc = Thread(target=receive)
+
+sd.start()
+rc.start()
+
+sd.join()
+rc.join()
+print("done")
