4fe52a4e65b177cfddc29cf8451476edd744b9b2
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2023-10-27 13:14:25 -0700

    added comment to hash.py

--- hash.py
+++ hash.py
@@ -1,11 +1,16 @@
 import hashlib
 
 def validator(data, hashed):
+    # convertin data to bytes
     bdata = data.encode("utf-8")
+    # creating hasher object
     hasher = hashlib.sha256()
+    # hashing data
     hasher.update(bdata)
+    # getting the hashed data in hexadecimal representation
     newhash = hasher.hexdigest()
-    print(newhash)
+    # if the hash is equal to received hash then the message is valid
+    # otherwise it is invalid
     if(newhash == hashed):
         return True
     else:
